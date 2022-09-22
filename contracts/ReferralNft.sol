3b955d6454997e1e9bf19b5d3d7b4c268f035faf
4metaverse <87619763+4metaverse@users.noreply.github.com>
2022-09-22 12:24:29 -0400

    add contracts

---
+++ contracts/ReferralNft.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract ReferralNft is ERC721URIStorage{
+
+    using Counters for Counters.Counter;
+    Counters.Counter public  _tokenIds;
+
+
+    constructor() ERC721("referrals", "RF") {
+
+    }
+
+    function mintNft(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
